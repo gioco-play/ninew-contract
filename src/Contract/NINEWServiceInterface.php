bc73d7f6bf0bacf8fbefbc8d3f8ed6bfb214b5a8
Shun <Ys.cheng@goldenf.co>
2023-07-10 10:39:29 +0800

    feat: update gameLaunch method

--- src/Contract/NINEWServiceInterface.php
+++ src/Contract/NINEWServiceInterface.php
@@ -8,10 +8,8 @@ interface NINEWServiceInterface {
      * @param string $op_code
      * @param string $account
      * @param string $game_code
-     * @param string $language
-     * @param string $vendor_code
-     * @param string $back_url
+     * @param ?string $back_url
      * @return array
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $vendor_code, string $back_url = null): array;
+    function gameLaunch(string $op_code, string $account, string $game_code, ?string $back_url = null): array;
 }
