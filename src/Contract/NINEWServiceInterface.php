6cccc0986b30a54b8b549bef9beac25e05a2cad9
Shun <Ys.cheng@goldenf.co>
2023-07-12 10:12:03 +0800

    feat: update gameLaunch/ + accountToVendor/ + accountToOperator

--- src/Contract/NINEWServiceInterface.php
+++ src/Contract/NINEWServiceInterface.php
@@ -5,11 +5,29 @@ namespace GiocoPlus\NINEW\Contract;
 interface NINEWServiceInterface {
     /**
      * 遊戲啟動
-     * @param string $op_code
+     * @param string $opCode
      * @param string $account
-     * @param string $game_code
-     * @param ?string $back_url
+     * @param string $gameCode
+     * @param ?string $backUrl
      * @return array
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, ?string $back_url = null): array;
+    function gameLaunch(string $opCode, string $account, string $gameCode, ?string $backUrl = null): array;
+
+    /**
+     * 營商帳號轉換為遊戲商帳號
+     * @param string $opCode
+     * @param array $vendor
+     * @param string $memberCode
+     * @return array
+     */
+    function accountToVendor(string $opCode, array $vendor, string $memberCode): array;
+
+    /**
+     * 遊戲商帳號轉換為營商帳號
+     * @param string $opCode
+     * @param array $vendor
+     * @param string $vendorAccount
+     * @return array
+     */
+    function accountToOperator(string $opCode, array $vendor, string $vendorAccount): array;
 }
